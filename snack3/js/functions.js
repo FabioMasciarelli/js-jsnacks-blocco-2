65f59a5a5f9c98532165ac257e03bd154c6d59b2
Fabio Masciarelli <fabiomasciarelliwd@gmail.com>
2024-04-05 14:19:30 +0200

    snack 3 finished

---
+++ snack3/js/functions.js
@@ -0,0 +1,10 @@
+function contaElementi (array) {
+
+    let num =0;
+
+    for (let i = 0; i < array.length; i++) {
+        num = array.length;
+    }
+
+    return num;
+}
--- snack3/js/script.js
+++ snack3/js/script.js
@@ -1,1 +1,16 @@
 // scrivere una funzione contaElementi() che, dato un array in input, restituisca il numero di elementi presenti nell'array
+
+const array =[];
+
+// chiedo all'utente di scrivere qualcosa
+let user = "";
+
+for (let i = 0; i < 10; i++) {
+
+    user = prompt("dimmi qualcosa");
+    array.push(user);
+    console.log(array);
+    const count = contaElementi(array);
+    console.log(count);
+}
+
